aa7996fea37128129154c545a5759dc7960066c8
Dragonwxl7 <73153172+Dragonwxl7@users.noreply.github.com>
2023-08-30 13:24:14 +0800

    Update api.ts
    delete interface url

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const GPT_URL = 'https://frontend.myhexin.com/kingfisher/robot/homeworkChat'
+const GPT_URL = ''
 
 export const chatGPT = (code:string,type?:string)=>{
   const params ={
     content:type ==='ai-explain' ? '请检查一下这段代码：' + code : code,
-    source:"homework-47-wangxiaolong",
-    token:"610EE45BF-Qtc2VydmU=",
+    source:"",
+    token:"",
     temperature: 1  
   }
   return axios.post(GPT_URL,params)
